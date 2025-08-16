a75073727b94b85d62ad3bef1af0067930178e70
MD.IBRAHIM KHAN <ibrahim2519@student.nstu.edu.bd>
2025-08-16 23:32:34 +0600

    1000rating-A_String_Task

---
+++ c++ problem/1000rating/A_String_Task.cpp
@@ -0,0 +1,27 @@
+//Bismillahir Rahmanir Rahim//
+//Author : Md. Ibrahim Khan//
+
+#include<bits/stdc++.h>
+#define ll long long
+#define srt(x) sort(x.begin(), x.end())
+#define rev(x) reverse(x.begin(), x.end())
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+   optimize();
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++){
+        if(s[i]!='A'&&s[i]!='E'&&s[i]!='I'&&s[i]!='O'&&s[i]!='U'&&s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u'&&s[i]!='Y'&&s[i]!='y'){
+            cout<<'.';
+            char c=tolower(s[i]);
+            cout<<c;
+        }
+    }
+
+
+
+return 0;
+}
