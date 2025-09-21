6ae4ba79f089b159161bf991e9c4d4b2ed1a2dc3
MD.IBRAHIM KHAN <ibrahim2519@student.nstu.edu.bd>
2025-09-21 09:27:31 +0600

    r

---
+++ c++ problem/1000rating/A_Blackboard_Game.cpp
@@ -0,0 +1,30 @@
+//Bismillahir Rahmanir Rahim//
+//Author : Md. Ibrahim Khan//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define testcase int t; cin >> t; for (int T = 1; T <= t; T++)
+#define ll long long
+#define srt(x) sort(x.begin(), x.end())
+#define rev(x) reverse(x.begin(), x.end())
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+void boss()
+{
+    int n;cin>>n;
+    
+    if(n%4==0){cout<<"Bob"<<"\n";}
+    else{cout<<"Alice"<<"\n";}
+
+
+}
+
+
+
+int main()
+{
+    optimize();
+    testcase
+    boss();
+    return 0;
+}
